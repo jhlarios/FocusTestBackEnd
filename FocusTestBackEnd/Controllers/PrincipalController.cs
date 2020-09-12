e4b50701807b8453b296fecfa3db7031570d7cdb
hector larios <hlarios7@hotmail.com>
2020-09-12 11:23:43 -0600

    Cambios en modelos  creacion de nuevos metodos

--- FocusTestBackEnd/Controllers/PrincipalController.cs
+++ FocusTestBackEnd/Controllers/PrincipalController.cs
@@ -15,14 +15,99 @@ namespace FocusTestBackEnd.Controllers
     [ApiController]
     public class PrincipalController : ControllerBase
     {
+        
+
+        // User methods
+
+        [HttpGet]
+        [Route("Users")]
+        public IActionResult Users(string id)
+        {
+
+
+            var Urlbase = "https://jsonplaceholder.typicode.com";
+            var UrlService = "/users";
+            if (id!=null && id!="")
+            {
+                UrlService += "?id=" + id;
+            }
+            var Data = new List<User> { };
+            try
+            {
+                HttpClient client = new HttpClient();
+                client.BaseAddress = new Uri(Urlbase);
+                var contentType = new MediaTypeWithQualityHeaderValue("application/json");
+                client.DefaultRequestHeaders.Accept.Add(contentType);
+                HttpResponseMessage response = client.GetAsync(UrlService).Result;
+                string stringJWT = response.Content.ReadAsStringAsync().Result;
+                Data = JsonConvert.DeserializeObject<List<User>>(stringJWT);
+
+            }
+            catch
+            {
+
+
+            }
+
+            return Ok(Data);
+        }
+
+
+
+
+        
+
+
+        // User albums
+
+        [HttpGet]
+        [Route("albums")]
+        public IActionResult albums(string id)
+        {
+
+
+            var Urlbase = "https://jsonplaceholder.typicode.com";
+            var UrlService = "/albums";
+            if (id != null && id != "")
+            {
+                UrlService += "?id=" + id;
+            }
+            var Data = new List<Album> { };
+            try
+            {
+                HttpClient client = new HttpClient();
+                client.BaseAddress = new Uri(Urlbase);
+                var contentType = new MediaTypeWithQualityHeaderValue("application/json");
+                client.DefaultRequestHeaders.Accept.Add(contentType);
+                HttpResponseMessage response = client.GetAsync(UrlService).Result;
+                string stringJWT = response.Content.ReadAsStringAsync().Result;
+                Data = JsonConvert.DeserializeObject<List<Album>>(stringJWT);
+
+            }
+            catch
+            {
+
+
+            }
+
+            return Ok(Data);
+        }
+
+
+        //Photo Methods
+
         [HttpGet]
         [Route("Photos")]
-        public IActionResult Photos()
+        public IActionResult Photos(string id)
         {
 
 
             var Urlbase = "https://jsonplaceholder.typicode.com";
             var UrlService = "/albums/1/photos";
+            if (id != null && id != "")
+            {
+                UrlService += "?id=" + id;
+            }
             var Data = new List<Photo> { };
             try
             {
@@ -45,34 +130,7 @@ namespace FocusTestBackEnd.Controllers
         }
 
 
-        [HttpGet]
-        [Route("PhotosDetail")]
-        public IActionResult PhotosDetail(string id)
-        {
-
-
-            var Urlbase = "https://jsonplaceholder.typicode.com";
-            var UrlService = "/albums/1/photos" + "?id=" + id;
-            var Data = new List<Photo> { };
-            try
-            {
-                HttpClient client = new HttpClient();
-                client.BaseAddress = new Uri(Urlbase);
-                var contentType = new MediaTypeWithQualityHeaderValue("application/json");
-                client.DefaultRequestHeaders.Accept.Add(contentType);
-                HttpResponseMessage response = client.GetAsync(UrlService).Result;
-                string stringJWT = response.Content.ReadAsStringAsync().Result;
-                Data = JsonConvert.DeserializeObject<List<Photo>>(stringJWT);
-
-            }
-            catch
-            {
-
-
-            }
-
-            return Ok(Data);
-        }
+       
 
 
         [Route("PhotoRegister")]
@@ -80,7 +138,7 @@ namespace FocusTestBackEnd.Controllers
         public IActionResult PhotoRegister(Photo Data)
         {
 
-           
+
             var Urlbase = "https://jsonplaceholder.typicode.com";
             var UrlService = "/albums/1/photos";
             HttpClient client = new HttpClient();
@@ -98,10 +156,77 @@ namespace FocusTestBackEnd.Controllers
             }
             catch
             {
-               
+
             }
             return Ok(Data);
         }
+
+
+        [Route("PhotoUpdate")]
+        [HttpPut]
+        public IActionResult PhotoUpdate(Photo Data)
+        {
+
+
+            var Urlbase = "https://jsonplaceholder.typicode.com";
+            var UrlService = "/albums/1/photos";
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(Urlbase);
+            try
+            {
+                var contentType = new MediaTypeWithQualityHeaderValue("application/json");
+                client.DefaultRequestHeaders.Accept.Add(contentType);
+                string stringData = JsonConvert.SerializeObject(Data);
+                var contentData = new StringContent(stringData, System.Text.Encoding.UTF8, "application/json");
+                HttpResponseMessage response = client.PutAsync(UrlService, contentData).Result;
+                string stringJWT = response.Content.ReadAsStringAsync().Result;
+                Data = JsonConvert.DeserializeObject<Photo>(stringJWT);
+
+            }
+            catch
+            {
+
+            }
+            return Ok(Data);
+        }
+
+
+        [Route("PhotoDelete")]
+        [HttpDelete]
+        public IActionResult PhotoDelete(string id)
+        {
+
+
+            var Urlbase = "https://jsonplaceholder.typicode.com";
+            var UrlService = "/albums/1/photos";
+            var Resultado = "";
+            if (id != null && id != "")
+            {
+                UrlService += "?id=" + id;
+            }
+            var Data = new List<Photo> { };
+            try
+            {
+                HttpClient client = new HttpClient();
+                client.BaseAddress = new Uri(Urlbase);
+                var contentType = new MediaTypeWithQualityHeaderValue("application/json");
+                client.DefaultRequestHeaders.Accept.Add(contentType);
+                HttpResponseMessage response = client.DeleteAsync(UrlService).Result;
+                string stringJWT = response.Content.ReadAsStringAsync().Result;
+                Data = JsonConvert.DeserializeObject<List<Photo>>(stringJWT);
+
+            }
+            catch
+            {
+
+
+            }
+        
+            return Ok(Resultado);
+        }
+
+
+
 
 
     }
--- FocusTestBackEnd/Models/User.cs
+++ FocusTestBackEnd/Models/User.cs
@@ -12,20 +12,25 @@ namespace FocusTestBackEnd.Models
         public string name { get; set; }
         public string username { get; set; }
         public string email { get; set; }
-        public string address { get; set; }
+        public string phone { get; set; }
+        public string website { get; set; }
+        public address address { get; set; }
+        public company company { get; set; }
+
+
+    }
+
+
+    public class address
+    {
         public string street { get; set; }
         public string suite { get; set; }
         public string city { get; set; }
         public string zipcode { get; set; }
-        public string phone { get; set; }
-        public string website { get; set; }
         public geo geo { get; set; }
-        public company company { get; set; }
-      
-
     }
 
-    public class geo
+public class geo
 
     {
         public string lat { get; set; }
