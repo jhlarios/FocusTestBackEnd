602fc01fcfc7b0e3dbd720cdcb0201290ff7c898
hector larios <hlarios7@hotmail.com>
2020-09-11 21:53:53 -0600

    Creacion de modelos

--- FocusTestBackEnd/Controllers/PrincipalController.cs
+++ FocusTestBackEnd/Controllers/PrincipalController.cs
@@ -21,8 +21,8 @@ namespace FocusTestBackEnd.Controllers
         {
 
 
-           var Urlbase = "https://jsonplaceholder.typicode.com";
-            var UrlService =  "/albums/1/photos";
+            var Urlbase = "https://jsonplaceholder.typicode.com";
+            var UrlService = "/albums/1/photos";
             var Data = new List<Photo> { };
             try
             {
@@ -33,16 +33,76 @@ namespace FocusTestBackEnd.Controllers
                 HttpResponseMessage response = client.GetAsync(UrlService).Result;
                 string stringJWT = response.Content.ReadAsStringAsync().Result;
                 Data = JsonConvert.DeserializeObject<List<Photo>>(stringJWT);
-                
+
             }
             catch
             {
 
-               
+
             }
 
             return Ok(Data);
         }
+
+
+        [HttpGet]
+        [Route("PhotosDetail")]
+        public IActionResult PhotosDetail(string id)
+        {
+
+
+            var Urlbase = "https://jsonplaceholder.typicode.com";
+            var UrlService = "/albums/1/photos" + "?id=" + id;
+            var Data = new List<Photo> { };
+            try
+            {
+                HttpClient client = new HttpClient();
+                client.BaseAddress = new Uri(Urlbase);
+                var contentType = new MediaTypeWithQualityHeaderValue("application/json");
+                client.DefaultRequestHeaders.Accept.Add(contentType);
+                HttpResponseMessage response = client.GetAsync(UrlService).Result;
+                string stringJWT = response.Content.ReadAsStringAsync().Result;
+                Data = JsonConvert.DeserializeObject<List<Photo>>(stringJWT);
+
+            }
+            catch
+            {
+
+
+            }
+
+            return Ok(Data);
+        }
+
+
+        [Route("PhotoRegister")]
+        [HttpPost]
+        public IActionResult PhotoRegister(Photo Data)
+        {
+
+           
+            var Urlbase = "https://jsonplaceholder.typicode.com";
+            var UrlService = "/albums/1/photos";
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(Urlbase);
+            try
+            {
+                var contentType = new MediaTypeWithQualityHeaderValue("application/json");
+                client.DefaultRequestHeaders.Accept.Add(contentType);
+                string stringData = JsonConvert.SerializeObject(Data);
+                var contentData = new StringContent(stringData, System.Text.Encoding.UTF8, "application/json");
+                HttpResponseMessage response = client.PostAsync(UrlService, contentData).Result;
+                string stringJWT = response.Content.ReadAsStringAsync().Result;
+                Data = JsonConvert.DeserializeObject<Photo>(stringJWT);
+
+            }
+            catch
+            {
+               
+            }
+            return Ok(Data);
+        }
+
 
     }
 }
---
+++ FocusTestBackEnd/Models/User.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FocusTestBackEnd.Models
+{
+    public class User
+    {
+
+        public int id { get; set; }
+        public string name { get; set; }
+        public string username { get; set; }
+        public string email { get; set; }
+        public string address { get; set; }
+        public string street { get; set; }
+        public string suite { get; set; }
+        public string city { get; set; }
+        public string zipcode { get; set; }
+        public string phone { get; set; }
+        public string website { get; set; }
+        public geo geo { get; set; }
+        public company company { get; set; }
+        pu
+
+    }
+
+    public class geo
+
+    {
+        public string lat { get; set; }
+        public string lng { get; set; }
+    }
+
+    public class company
+    {
+        public string name { get; set; }
+        public string catchPhrase { get; set; }
+        public string bs { get; set; }
+    }
+
+
+}
